7cf9541bc29b64d19559e2c972ced0517e4fab3b
OngurapusEmmanuel <emmanuelongurapus@gmail.com>
2024-12-01 23:12:25 +0300

    website update

--- js/modal.js
+++ js/modal.js
@@ -5,3 +5,15 @@ window.onload = function() {
 function closeModal() {
     document.getElementById('popup').style.display = 'none';
 }
+
+// document.querySelector('.btn').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     document.getElementById('register-modal').style.display = 'flex';
+// });
+function registerModal() {
+    document.getElementById('register-modal').style.display = 'flex';
+}
+
+function closeRegisterModal() {
+    document.getElementById('register-modal').style.display = 'none';
+}
