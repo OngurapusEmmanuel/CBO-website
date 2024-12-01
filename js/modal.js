38a27f18aca649bd6be158e1a16f947604e95a48
OngurapusEmmanuel <emmanuelongurapus@gmail.com>
2024-12-01 22:52:53 +0300

    website update

---
+++ js/modal.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.getElementById('popup').style.display = 'flex';
+};
+
+function closeModal() {
+    document.getElementById('popup').style.display = 'none';
+}
